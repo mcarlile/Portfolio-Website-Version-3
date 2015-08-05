b93028f7d3d1b2c585cab94324fa93423edd01d6
mcarlile <mckenzie.carlile@gmail.com>
2015-08-05 16:36:05 -0500

    Sets height of the slider according to height of the slide

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
-    $('.slider').slick({
-        dots: true,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        adaptiveHeight: false,
-    });
+	$('.slider').slick({
+		dots: true,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 1,
+		adaptiveHeight: false,
+	});
 });
+
+console.log("Slider was: " + $('.slider').height());
+var slideHeight;
+slideHeight = $('.slide').height();
+$('.slider').height(slideHeight);
+console.log("Slider is now: " + $('.slider').height());
