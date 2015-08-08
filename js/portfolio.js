7bebde47a4abab61a335e78b78774ce4738d58b4
mcarlile <mckenzie.carlile@gmail.com>
2015-08-07 20:47:41 -0500

    Got slider and description text to work as wanted, added first favicon

--- js/portfolio.js
+++ js/portfolio.js
@@ -5,11 +5,28 @@ $(document).ready(function(){
 		speed: 300,
 		slidesToShow: 1,
 		adaptiveHeight: false,
+		autoplay: true,
+		autoplaySpeed: 4000,
+		fade: false,
+		cssEase: 'linear'
 	});
 });
 
-console.log("Slider was: " + $('.slider').height());
-var slideHeight;
-slideHeight = $('.slide').height();
-$('.slider').height(slideHeight);
-console.log("Slider is now: " + $('.slider').height());
+// console.log("Slider was: " + $('.slider').height());
+// var slideHeight;
+// slideHeight = $('.slide').height();
+// $('.slider').height(slideHeight);
+// console.log("Slider is now: " + $('.slider').height());
+
+$('.slider').on('afterChange', function(slick, currentSlide, nextSlide){
+	var slideInQuestion = currentSlide.$slides.get(currentSlide.currentSlide).getAttribute('id');
+	console.log(slideInQuestion);
+
+	if (slideInQuestion == 'mini') {
+		$('h2#projectName').text("Mini Cooper Lifelog Project");
+	} else if (slideInQuestion == 'crossfit') {
+		$('h2#projectName').text("Crossfit Design Exercise");
+	} else {
+		$('h2#projectName').text("Disney Interactive Internship Project");
+	}
+});
